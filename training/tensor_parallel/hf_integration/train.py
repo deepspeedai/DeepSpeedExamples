1846518a73d3a9a617276810adbd5338e7c0d97f
Masahiro Tanaka <mtanaka@anyscale.com>
2026-02-01 10:18:51 -0800

    replace deprecated openai package
    Signed-off-by: Masahiro Tanaka <mtanaka@anyscale.com>

--- training/tensor_parallel/hf_integration/train.py
+++ training/tensor_parallel/hf_integration/train.py
@@ -19,7 +19,7 @@ from typing import Dict, Optional, Sequence
 
 import torch
 import transformers
-import utils
+import json
 from torch.utils.data import Dataset
 from transformers import Trainer
 
@@ -131,7 +131,7 @@ class SupervisedDataset(Dataset):
     def __init__(self, data_path: str, tokenizer: transformers.PreTrainedTokenizer):
         super(SupervisedDataset, self).__init__()
         logging.warning("Loading data...")
-        list_data_dict = utils.jload(data_path)
+        list_data_dict = json.load(open(data_path))
 
         logging.warning("Formatting inputs...")
         prompt_input, prompt_no_input = PROMPT_DICT["prompt_input"], PROMPT_DICT["prompt_no_input"]
